81a318b1524bba2b3472cf70646fb35be323497d
kingbri <bdashore3@gmail.com>
2020-05-21 11:05:31 -0400

    Add new commands from textbender
    Add commands: upp, uppl, low, lowl, inv, invl
    
    upp, uppl: Make the entire message uppercase
    low, lowl: Make the entire message lowercase
    inv, invl: Invert the case of each letter of the message
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- src/commands/textmod.rs
+++ src/commands/textmod.rs
@@ -30,7 +30,7 @@ pub async fn mockl(ctx: &Context, msg: &Message) -> CommandResult {
     Ok(())
 }
 
-pub fn get_mock_string(input: &str) -> String {
+fn get_mock_string(input: &str) -> String {
 
     let mut output = String::with_capacity(input.len());
 
@@ -44,4 +44,82 @@ pub fn get_mock_string(input: &str) -> String {
     }
 
     output
+}
+
+#[command]
+async fn inv(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    let inv_string = get_inverted_string(args.rest());
+    msg.channel_id.say(&ctx.http, inv_string).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn invl(ctx: &Context, msg: &Message) -> CommandResult {
+
+    let input_message = msg.channel_id.messages(&ctx.http, |retriever| {
+        retriever.before(msg.id).limit(1)
+    })
+    .await?;
+    
+    let inv_string = get_inverted_string(&input_message[0].content);
+
+    msg.channel_id.say(&ctx.http, inv_string).await?;
+
+    Ok(())
+}
+
+
+fn get_inverted_string(input: &str) -> String {
+    
+    let mut output = String::with_capacity(input.len());
+
+    for x in input.chars() {
+        if x.is_uppercase() {
+            output.push(x.to_lowercase().collect::<Vec<_>>()[0]);
+        }
+        else if x.is_lowercase() {
+            output.push(x.to_uppercase().collect::<Vec<_>>()[0]);
+        }
+    }
+
+    output
+}
+
+#[command]
+async fn upp(ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    msg.channel_id.say(&ctx.http, args.rest().to_uppercase()).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn uppl(ctx: &Context, msg: &Message) -> CommandResult {
+    let input_message = msg.channel_id.messages(&ctx.http, |retriever| {
+        retriever.before(msg.id).limit(1)
+    })
+    .await?;
+
+    msg.channel_id.say(&ctx.http, input_message[0].content.to_uppercase()).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn low (ctx: &Context, msg: &Message, args: Args) -> CommandResult {
+    msg.channel_id.say(&ctx.http, args.rest().to_lowercase()).await?;
+
+    Ok(())
+}
+
+#[command]
+async fn lowl(ctx: &Context, msg: &Message) -> CommandResult {
+    let input_message = msg.channel_id.messages(&ctx.http, |retriever| {
+        retriever.before(msg.id).limit(1)
+    })
+    .await?;
+
+    msg.channel_id.say(&ctx.http, input_message[0].content.to_lowercase()).await?;
+
+    Ok(())
 }
