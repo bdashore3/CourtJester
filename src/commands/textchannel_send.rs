67df71e41567d66e9b644f5e1acf102c0ea2f92d
kingbri <bdashore3@gmail.com>
2020-05-29 11:08:41 -0400

    Add permissions and fix some bugs
    Add permission restrictions for certain commands and certain parts
    of a command.
    
    For example, if I want to set the channel in a command where there
    are no subcommands, the user's permissions are checked in permissions_handler.
    
    However, if there's a command that's completely restricted to a certain
    permission, we use serenity's inbuilt framework to check the permission.
    
    In addition, Serenity currently has a bug where parsing an argument
    into a ChannelId also works with a UserId and therefore breaks the
    storage mechanism. Instead, use parse_channel along with casting
    the argument to a String.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- src/commands/textchannel_send.rs
+++ src/commands/textchannel_send.rs
@@ -5,38 +5,44 @@ use serenity::framework::standard::{
     macros::command,
     Args
 };
+use serenity::utils::*;
 
 use sqlx;
 use crate::ConnectionPool;
-use crate::helpers::textmod_helper;
+use crate::helpers::*;
 
 #[command]
 async fn nice(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    if !args.is_empty() {
-        msg.channel_id.say(ctx, "Please execute this command without any arguments").await?;
+    let guild_id = msg.guild_id.unwrap();
+
+    let test_id = args.single::<String>().unwrap_or_default();
+
+    if let Some(channel_id) = parse_channel(&test_id) {
+        if permissions_helper::check_permission(ctx, msg, Permissions::ADMINISTRATOR).await {
+
+            let data = ctx.data.read().await;
+
+            let pool = data.get::<ConnectionPool>().unwrap();
+
+            let check = sqlx::query!("SELECT EXISTS(SELECT 1 FROM text_channels WHERE guild_id = $1)", guild_id.0 as i64)
+                .fetch_one(pool)
+                .await?;
+        
+            if check.exists.unwrap() {
+                sqlx::query!("UPDATE text_channels SET nice_id = $1 WHERE guild_id = $2", channel_id as i64, guild_id.0 as i64)
+                    .execute(pool).await?;
+            } else {
+                textmod_helper::create_channel_row(ctx, guild_id.0 as i64, channel_id as i64, None, None).await?;
+            }
+        
+            msg.channel_id.say(ctx, "Channel sucessfully set!").await?;
+        }
+
         return Ok(())
     }
 
-    let guild_id = msg.guild_id.unwrap();
-
-    if let Ok(channel_id) = args.single::<ChannelId>() {
-        let data = ctx.data.read().await;
-
-        let pool = data.get::<ConnectionPool>().unwrap();
-
-        let check = sqlx::query!("SELECT EXISTS(SELECT 1 FROM text_channels WHERE guild_id = $1)", guild_id.0 as i64)
-            .fetch_one(pool)
-            .await?;
-        
-        if check.exists.unwrap() {
-            sqlx::query!("UPDATE text_channels SET nice_id = $1 WHERE guild_id = $2", channel_id.0 as i64, guild_id.0 as i64)
-                .execute(pool).await?;
-        } else {
-            textmod_helper::create_channel_row(ctx, guild_id.0 as i64, channel_id.0 as i64, None, None).await?;
-        }
-        
-        msg.channel_id.say(ctx, "Channel sucessfully set!").await?;
-
+    if !args.is_empty() {
+        msg.channel_id.say(ctx, "Please execute this command without any arguments").await?;
         return Ok(())
     }
 
@@ -54,31 +60,36 @@ async fn nice(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 
 #[command]
 async fn bruh(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    if !args.is_empty() {
-        msg.channel_id.say(ctx, "Please execute this command without any arguments").await?;
+    let guild_id = msg.guild_id.unwrap();
+
+    let test_id = args.single::<String>().unwrap_or_default();
+
+    if let Some(channel_id) = parse_channel(&test_id) {
+        if permissions_helper::check_permission(ctx, msg, Permissions::ADMINISTRATOR).await {
+
+            let data = ctx.data.read().await;
+
+            let pool = data.get::<ConnectionPool>().unwrap();
+
+            let check = sqlx::query!("SELECT EXISTS(SELECT 1 FROM text_channels WHERE guild_id = $1)", guild_id.0 as i64)
+                .fetch_one(pool)
+                .await?;
+        
+            if check.exists.unwrap() {
+                sqlx::query!("UPDATE text_channels SET bruh_id = $1 WHERE guild_id = $2", channel_id as i64, guild_id.0 as i64)
+                    .execute(pool).await?;
+            } else {
+                textmod_helper::create_channel_row(ctx, guild_id.0 as i64, None, channel_id as i64, None).await?;
+            }
+        
+            msg.channel_id.say(ctx, "Channel sucessfully set!").await?;
+        }
+
         return Ok(())
     }
 
-    let guild_id = msg.guild_id.unwrap();
-
-    if let Ok(channel_id) = args.single::<ChannelId>() {
-        let data = ctx.data.read().await;
-
-        let pool = data.get::<ConnectionPool>().unwrap();
-
-        let check = sqlx::query!("SELECT EXISTS(SELECT 1 FROM text_channels WHERE guild_id = $1)", guild_id.0 as i64)
-            .fetch_one(pool)
-            .await?;
-        
-        if check.exists.unwrap() {
-            sqlx::query!("UPDATE text_channels SET bruh_id = $1 WHERE guild_id = $2", channel_id.0 as i64, guild_id.0 as i64)
-                .execute(pool).await?;
-        } else {
-            textmod_helper::create_channel_row(ctx, guild_id.0 as i64, None, channel_id.0 as i64, None).await?;
-        }
-        
-        msg.channel_id.say(ctx, "Channel sucessfully set!").await?;
-
+    if !args.is_empty() {
+        msg.channel_id.say(ctx, "Please execute this command without any arguments").await?;
         return Ok(())
     }
 
@@ -100,28 +111,38 @@ async fn quote(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
     let guild_id = msg.guild_id.unwrap();
     let mut self_quote = true;
 
-    if args.single::<UserId>().is_ok() {
+    let test_id = args.single::<String>().unwrap_or_default();
+
+    if let Some(channel_id) = parse_channel(&test_id) {
+        if permissions_helper::check_permission(ctx, msg, Permissions::ADMINISTRATOR).await {
+
+            let data = ctx.data.read().await;
+
+            let pool = data.get::<ConnectionPool>().unwrap();
+
+            let check = sqlx::query!("SELECT EXISTS(SELECT 1 FROM text_channels WHERE guild_id = $1)", guild_id.0 as i64)
+            .fetch_one(pool)
+                .await?;
+        
+            if check.exists.unwrap() {
+                sqlx::query!("UPDATE text_channels SET quote_id = $1 WHERE guild_id = $2", channel_id as i64, guild_id.0 as i64)
+                    .execute(pool).await?;
+            } else {
+                textmod_helper::create_channel_row(ctx, guild_id.0 as i64, None, channel_id as i64, None).await?;
+            }
+        
+            msg.channel_id.say(ctx, "Channel sucessfully set!").await?;
+        }
+
+        return Ok(())
+    }
+
+    if parse_username(&test_id).is_some() {
         self_quote = false;
     }
 
-    if let Ok(channel_id) = args.single::<ChannelId>() {
-        let data = ctx.data.read().await;
-
-        let pool = data.get::<ConnectionPool>().unwrap();
-
-        let check = sqlx::query!("SELECT EXISTS(SELECT 1 FROM text_channels WHERE guild_id = $1)", guild_id.0 as i64)
-            .fetch_one(pool)
-            .await?;
-        
-        if check.exists.unwrap() {
-            sqlx::query!("UPDATE text_channels SET quote_id = $1 WHERE guild_id = $2", channel_id.0 as i64, guild_id.0 as i64)
-                .execute(pool).await?;
-        } else {
-            textmod_helper::create_channel_row(ctx, guild_id.0 as i64, None, channel_id.0 as i64, None).await?;
-        }
-        
-        msg.channel_id.say(ctx, "Channel sucessfully set!").await?;
-
+    if args.is_empty() || test_id == "" {
+        msg.channel_id.say(ctx, "Please provide the quote (with author if you are quoting someone else)").await?;
         return Ok(())
     }
 
--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,3 +1,4 @@
 pub mod credentials_helper;
 pub mod textmod_helper;
 pub mod database_helper;
+pub mod permissions_helper;
