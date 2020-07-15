b63c38998582bb530a093fa5571dfee64a06e507
kingbri <bdashore3@gmail.com>
2020-07-15 00:01:31 -0400

    Add sqlx migrations
    Allow us to populate a new database with the right configuration after
    cloning this repo
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

---
+++ migrations/2020-07-03_14-57-21_starbot.sql
@@ -0,0 +1,40 @@
+-- Add migration script here
+CREATE TABLE public.starbot
+(
+    guild_id bigint NOT NULL,
+    reaction_message_id bigint NOT NULL,
+    sent_message_id bigint NOT NULL,
+    CONSTRAINT starbot_pkey PRIMARY KEY (guild_id, reaction_message_id),
+    CONSTRAINT "FK_commands_guild_info_guild_id" FOREIGN KEY (guild_id)
+        REFERENCES public.guild_info (guild_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.starbot
+    OWNER to postgres;
+
+ALTER TABLE public.guild_info
+    ADD COLUMN starbot_threshold integer;
+
+DROP TABLE public.text_channels;
+
+CREATE TABLE public.text_channels
+(
+    guild_id bigint NOT NULL,
+    nice_id bigint,
+    bruh_id bigint,
+    quote_id bigint,
+    CONSTRAINT text_channels_pkey PRIMARY KEY (guild_id),
+    CONSTRAINT "FK_text_channels_guild_info_guild_id" FOREIGN KEY (guild_id)
+        REFERENCES public.guild_info (guild_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE CASCADE
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.text_channels
+    OWNER to postgres;
