3d125682f07b7991866e9b2cece9f5b39036bc3c
kingbri <bdashore3@gmail.com>
2020-07-17 11:31:23 -0400

    Add basic voice join/leave commands
    Also add in initial support for lavalink playback
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- src/structures/mod.rs
+++ src/structures/mod.rs
@@ -1,9 +1,10 @@
 use serenity::{
-    client::bridge::gateway::ShardManager, 
+    client::bridge::{voice::ClientVoiceManager, gateway::ShardManager}, 
     prelude::{Mutex, TypeMapKey}
 };
 use std::sync::Arc;
 use sqlx::PgPool;
+use serenity_lavalink::LavalinkClient;
 
 // All command context data structures
 pub struct ShardManagerContainer;
@@ -22,4 +23,16 @@ pub struct DefaultPrefix;
 
 impl TypeMapKey for DefaultPrefix {
     type Value = Arc<String>;
+}
+
+pub struct VoiceManager;
+
+impl TypeMapKey for VoiceManager {
+    type Value = Arc<Mutex<ClientVoiceManager>>;
+}
+
+pub struct Lavalink;
+
+impl TypeMapKey for Lavalink {
+    type Value = LavalinkClient;
 }
