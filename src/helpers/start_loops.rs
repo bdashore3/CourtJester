7c0916ae84c9db69fdbea43527a0363062608540
kingbri <bdashore3@gmail.com>
2020-12-27 12:42:06 -0500

    Startup: Add a looping activity
    The bot now loops activities on Discord. This is achieved by
    a simple loop with a delay of 4 hours between each activity change.
    
    There are currently 7 activities with more that will be added
    in the future.
    
    Resolves #12
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- src/helpers/start_loops.rs
+++ src/helpers/start_loops.rs
@@ -1,16 +1,15 @@
-use serenity::{framework::standard::CommandResult, model::id::GuildId, prelude::*};
+use rand::{Rng, SeedableRng, prelude::StdRng};
+use serenity::{client::bridge::gateway::ShardMessenger, framework::standard::CommandResult, model::{id::GuildId, prelude::Activity}, prelude::*};
+use sqlx::PgPool;
 use crate::ConnectionPool;
 use std::time::{SystemTime, UNIX_EPOCH, Duration};
 use tokio::time::delay_for;
 
 
-pub async fn starboard_removal_loop(ctx: Context) -> CommandResult {
-    let pool = ctx.data.read().await
-        .get::<ConnectionPool>().cloned().unwrap();
-
+pub async fn starboard_removal_loop(pool: &PgPool) -> CommandResult {
     loop {    
         let delete_data = sqlx::query!("SELECT guild_id, reaction_message_id, sent_message_id, delete_time FROM starboard")
-            .fetch_all(&pool).await?;
+            .fetch_all(pool).await?;
         
         for i in delete_data {
             println!("Checking delete status on starboard message {}", i.guild_id);
@@ -24,7 +23,7 @@ pub async fn starboard_removal_loop(ctx: Context) -> CommandResult {
                 println!("Deleting guild {} from the database \n", i.guild_id);
                 sqlx::query!("DELETE FROM starboard WHERE guild_id = $1 AND reaction_message_id = $2 AND sent_message_id = $3",
                         i.guild_id, i.reaction_message_id, i.sent_message_id)
-                    .execute(&pool).await?;
+                    .execute(pool).await?;
             } else {
                 println!("Entry's time isn't greater than a week! Not deleting guild {}! \n", i.guild_id);
             }
@@ -57,4 +56,29 @@ pub async fn guild_pruner(ctx: &Context) -> CommandResult {
     println!(" ");
 
     Ok(())
+}
+
+pub async fn activity_loop(messenger: &ShardMessenger) {
+    let activity_vec = vec![
+        Activity::playing("as the fool"),
+        Activity::listening("a tune!"),
+        Activity::listening("straight vibes"),
+        Activity::playing("vibe checks"),
+        Activity::playing("hacking the mainframe"),
+        Activity::playing("boof simulator 2021"),
+        Activity::playing("ZA WARUDO!"),
+        Activity::listening("lofi music"),
+        Activity::playing("Minecraft"),
+        Activity::listening("Bhai tunes")
+    ];
+
+    let mut rng = StdRng::from_entropy();
+
+    loop {
+        let val = rng.gen_range(0..=activity_vec.len() - 1);
+
+        messenger.set_activity(Some(activity_vec[val].to_owned()));
+
+        delay_for(Duration::from_secs(7200)).await;
+    }
 }
