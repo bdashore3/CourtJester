58ea0c547537253b7f24492874260ea3aa64f257
kingbri <bdashore3@gmail.com>
2021-02-22 09:37:22 -0500

    Update dependencies and cargo fmt
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- src/structures/errors.rs
+++ src/structures/errors.rs
@@ -5,16 +5,16 @@ use std::fmt;
 pub enum JesterError<'a> {
     PermissionError(PermissionType<'a>),
     MissingError(&'a str),
-    UnsuccessfulError(&'a str)
+    UnsuccessfulError(&'a str),
 }
 
-impl fmt::Display for JesterError <'_> {
+impl fmt::Display for JesterError<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            JesterError::PermissionError(perm) => 
+            JesterError::PermissionError(perm) =>
                 write!(f, "{}", perm),
             JesterError::MissingError(missing) => write!(f, "Please provide a {}!", missing),
-            JesterError::UnsuccessfulError(cmd) => 
+            JesterError::UnsuccessfulError(cmd) =>
                 write!(f, "{} unsuccessful. The user must be in the guild and the bot must be above the user's role!", cmd)
         }
     }
@@ -24,14 +24,22 @@ impl fmt::Display for JesterError <'_> {
 #[derive(Clone, Copy, Debug)]
 pub enum PermissionType<'b> {
     SelfPerm(&'b str),
-    Mention(&'b str, &'b str)
+    Mention(&'b str, &'b str),
 }
 
-impl fmt::Display for PermissionType <'_> {
+impl fmt::Display for PermissionType<'_> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            PermissionType::SelfPerm(perm) => writeln!(f, "You can't execute this command because you're not a {} on this server!", perm),
-            PermissionType::Mention(cmd, perm) => write!(f, "I can't {} an {}! Please demote the user and try again", cmd, perm)
+            PermissionType::SelfPerm(perm) => writeln!(
+                f,
+                "You can't execute this command because you're not a {} on this server!",
+                perm
+            ),
+            PermissionType::Mention(cmd, perm) => write!(
+                f,
+                "I can't {} an {}! Please demote the user and try again",
+                cmd, perm
+            ),
         }
     }
 }
